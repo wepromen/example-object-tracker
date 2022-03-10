fae27501648391819067d59dc9645cfdfe639e9b
Nam Nguyen <namnh@unicloud.com.vn>
2022-03-10 17:32:39 +0700

    A

--- gstreamer/gstreamer.py
+++ gstreamer/gstreamer.py
@@ -254,7 +254,7 @@ def run_pipeline(user_function,
             t. ! {leaky_q} ! videoconvert ! videoscale ! {scale_caps} ! videobox name=box autocrop=true
                ! {sink_caps} ! {sink_element}
             t. ! {leaky_q} ! videoconvert
-               ! rsvgoverlay name=overlay ! videoconvert ! jpegenc ! tcpclientsink host=127.0.0.1 port=9001
+               ! rsvgoverlay name=overlay ! videoconvert ! jpegenc ! udpsink host=127.0.0.1 port=5700
             """
             #ximagesink sync=false
     if objectOfTracker:
