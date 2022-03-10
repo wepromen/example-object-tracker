5d3191b85a38b551efe730786ae871b3abd998ab
Nam Nguyen <namnh@unicloud.com.vn>
2022-03-10 16:47:04 +0700

    _

--- gstreamer/gstreamer.py
+++ gstreamer/gstreamer.py
@@ -247,9 +247,9 @@ def run_pipeline(user_function,
     #         t. ! queue ! glsvgoverlaysink name=overlaysink
     #     """
     # else:
-        scale = min(appsink_size[0] / src_size[0], appsink_size[1] / src_size[1])
-        scale = tuple(int(x * scale) for x in src_size)
-        scale_caps = 'video/x-raw,width={width},height={height}'.format(width=scale[0], height=scale[1])
+    scale = min(appsink_size[0] / src_size[0], appsink_size[1] / src_size[1])
+    scale = tuple(int(x * scale) for x in src_size)
+    scale_caps = 'video/x-raw,width={width},height={height}'.format(width=scale[0], height=scale[1])
         # PIPELINE += """ ! tee name=t
         #     t. ! {leaky_q} ! videoconvert ! videoscale ! {scale_caps} ! videobox name=box autocrop=true
         #        ! {sink_caps} ! {sink_element}
